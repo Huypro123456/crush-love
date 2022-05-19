841d263eefffd56f2bc218a0511a37740d5f085d
Huypro123456 <105871074+Huypro123456@users.noreply.github.com>
2022-05-19 21:54:23 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Làm người yêu tớ nhé I Love You",
+  titleHeader: "Làm người yêu t nhé =)))",
   descriptionHeader:
-    "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
-  buttonYes: "Đồng Ý",
-  buttonNo: "Không! Còn lâu nhé.",
-  titleModar: "Cảm ơn em vì đồng ý",
+    "Nếu cậu thoát ra và không trả lời thì có nghĩa là bà đã ĐỒNG Ý rồi đấy =))) bị scam kìa",
+  buttonYes: "Sao lại ko nhỉ",
+  buttonNo: "Không! Còn lâu nhá.",
+  titleModar: "Cảm ơn bà đã đồng ý =)))",
   descriptionModar:
-    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+    'T biết là bà sẽ đồng ý làm người yêu t mà ^^ </br> Cám ơn bà đã đồng ý để cho t cơ hôi nhá =))).Đối với t, bà mãi cute =)))) </br> "yew bà nhiều đến mấy, bà sẽ hiểu thôi. </br>  T thích bà nhiều đến mấy,bà sẽ hiểu thôi =)))"',
 };
